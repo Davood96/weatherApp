88ce22b77c079ac75b37f388d8ae4afac70663d1
Davood Anbarnam <danbarna@uwaterloo.ca>
2020-06-15 17:01:12 -0400

    Added comments outlining the improvements that can be made

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
-
-
-// const ecweather = require('ec-weather');
+// Using OpenWeatherMap API instead of Env. Canada RSS Feed
+// because:
+// a) Parsing the RSS Feed was becoming too messy
+// b) Matching condition descriptions to an icon was becoming cumbersome.
+//    The OpenWeatherMap API provides these icons out of the box
 const weather = require('openweather-apis');
 const weather_API_KEY = require('./config.js');
 weather.setLang('en');
 weather.setUnits('metric');
 weather.setAPPID(weather_API_KEY);
+
 const express = require('express');
-
 const app = express();
-app.use(express.static(__dirname = "public"));
+app.use(express.static(__dirname + "/public"));
 
+// This should be defined in a model module instead
+// of hardcoding it here
 const locations = [ 
                   'Dease Lake', 'Fort Nelson', 'Terrace', 'Prince George', 
                   'Whistler', 'Revelstoke', 'Creston'
                 ];
-
+// Should use a more meaningful request url that specifies
+// the intended behaviour.
 app.get('/sse', (req, res) => {
     console.log("Sending Event!");
+    // Should set an 'event' field to specify the event type
+    // e.g., currentCondition.
     res.status(200).set({
         "connection" : "keep-alive",
         "cache-control" : "no-cache",
         "content-Type" : "text/event-stream"
     });
 
-    // Sending new data
+    // Sending new data on current conditions
     locations.map((location) => {
         weather.setCity(location + ", CA");
+        // Should define an interface for the data being sent in a 'models'
+        // module
         weather.getAllWeather((err, result) => {
             let data = { cityName : result.name, weather : result.weather[0], temp : result.main.temp }
             res.write('data: ' + JSON.stringify(data) + '\n\n');
@@ -40,5 +49,5 @@ var server = app.listen(3000, function () {
     var host = server.address().address
     var port = server.address().port
     
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("Listening at http://%s:%s", host, port)
 });
--- public/controller.js
+++ public/controller.js
@@ -1,153 +1,87 @@
 
+$(document).ready(function () {
 
-const BC_geocode = {lat:53.7266683,lng:-127.6476205}
-const geocoder = new google.maps.Geocoder();
-let myStyle = [
-    {
-        "featureType" : "administrative.locality",
-        "stylers" : [{"visibility" : "off"}]
-    },
-    {
-        "featureType" : "administrative.neighborhood",
-        "stylers" : [{"visibility" : "off"}]
-    },
-    {
-        "featureType" : "road",
-        "stylers" : [{"visibility" : "off"}]
-    }
-]
-let mapProp = {
-    center:BC_geocode,
-    zoom:5,
-    styles : myStyle
-};
+    // Should use the Gecode API to get this instead of
+    // hardcoding it
+    const BC_geocode = {lat:53.7266683,lng:-127.6476205};
+    const geocoder = new google.maps.Geocoder();
+    let myStyle = [
+        {
+            "featureType" : "administrative.locality",
+            "stylers" : [{"visibility" : "off"}]
+        },
+        {
+            "featureType" : "administrative.neighborhood",
+            "stylers" : [{"visibility" : "off"}]
+        },
+        {
+            "featureType" : "road",
+            "stylers" : [{"visibility" : "off"}]
+        }
+    ]
+    const mapProp = {
+        center:BC_geocode,
+        zoom:5,
+        styles : myStyle
+    };
+    const globMap = new google.maps.Map($("#googleMap")[0], mapProp);
+    // Maps city names to their markers on the map
+    // Ideally, the key should consist of the city name, country and state (if applicable)
+    // Also, an interface for the values being stored should be explicitly defined
+    const weatherMarkers = {}
 
-var globMap = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-// new google.maps.Marker({
-//     position : BC_geocode,
-//     map : globMap,
-//     label : { text: "BC" },
-//     icon : { url : '',
-//         scaledSize : new google.maps.Size(0,0),
-//         labelOrigin : new google.maps.Point(0, 10)
-//     }
-// })
-var weatherMarkers = {}
+    const evntSource = new EventSource('/sse');
+    // Should define event handlers for different event types,
+    // instead of this global handler
+    evntSource.onmessage = (event) => {
 
-const evntSource = new EventSource('/sse');
-evntSource.onmessage = (event) => {
-    // alert(event.data);
-    let weatherData = JSON.parse(event.data);
-    if(!weatherMarkers.hasOwnProperty(weatherData.cityName))
-    {
-        // alert("Add Marker for " + weatherData.cityName);
-            geocoder.geocode({'address': weatherData.cityName + ",BC"}, (results, status) => {
-            // alert(status);
-            let marker = new google.maps.Marker({
-                    position : results[0].geometry.location,
-                    map : globMap,
+        let weatherData = JSON.parse(event.data);
+        if(!weatherMarkers.hasOwnProperty(weatherData.cityName))
+        {
+                geocoder.geocode({'address': weatherData.cityName + ",BC"}, (results, status) => {
+                let marker = new google.maps.Marker({
+                        position : results[0].geometry.location,
+                        map : globMap,
+                        icon : {
+                                // Should define a function that returns the URL below instead of hardcoding it
+                                url : "http://openweathermap.org/img/w/" + weatherData.weather.icon + ".png",
+                                labelOrigin : new google.maps.Point(40,5),
+                            },
+                        label : {text : weatherData.temp.toString() + "°C"} 
+                });
+                let locationLabel = new google.maps.Marker({
                     icon : { 
-                            url : "http://openweathermap.org/img/w/" + weatherData.weather.icon + ".png",
-                            labelOrigin : new google.maps.Point(40,5),
+                                url : '', 
+                                scaledSize : new google.maps.Size(0,0),
+                                labelOrigin : new google.maps.Point(0, 0)
                         },
-                    label : {text : weatherData.temp.toString() + "°C"} 
+                    position : marker.getPosition(),
+                    map : globMap,
+                    label : {text : weatherData.cityName }
+                })
+                weatherMarkers[weatherData.cityName] = 
+                    {   temp : weatherData.temp, 
+                        condition : weatherData.weather.icon, 
+                        marker : marker
+                    };
             });
-            let locationLabel = new google.maps.Marker({
-                icon : { 
-                            url : '', 
-                            scaledSize : new google.maps.Size(0,0),
-                            labelOrigin : new google.maps.Point(0, 0)
-                       },
-                position : marker.getPosition(),
-                map : globMap,
-                label : {text : weatherData.cityName }
-            })
-            weatherMarkers[weatherData.cityName] = 
-                {   temp : weatherData.temp, 
-                    condition : weatherData.weather.icon, 
-                    marker : marker
-                };
-            // alert(JSON.stringify(weatherMarkers));
-        });
-    }
-    else
-    {
-        // alert("Updating Data!")
-        if(weatherData.weather.temp !== weatherMarkers[weatherData.cityName].temp)
-        {
-            weatherMarkers[weatherData.cityName].temp = weatherData.weather.temp;
-            weatherMarkers[weatherData.cityName].marker.setLabel(weatherData.weather.temp.toString() + "°C");
         }
-        if(weatherData.weather.icon !== weatherMarkers[weatherData.cityName].condition)
+        else
         {
-            alert("Changing Condition!");
-            weatherMarkers[weatherData.cityName].condition = weatherData.weather.icon;
-            let icon = weatherMarkers[weatherData.cityName].marker.getIcon();
-            icon.url = "http://openweathermap.org/img/w/" + weatherData.weather.icon + ".png";
-            weatherMarkers[weatherData.cityName].marker.setIcon(icon);
+            if(weatherData.weather.temp !== weatherMarkers[weatherData.cityName].temp)
+            {
+                weatherMarkers[weatherData.cityName].temp = weatherData.temp;
+                weatherMarkers[weatherData.cityName].marker.setLabel(weatherData.temp.toString() + "°C");
+            }
+            if(weatherData.weather.icon !== weatherMarkers[weatherData.cityName].condition)
+            {
+                weatherMarkers[weatherData.cityName].condition = weatherData.weather.icon;
+                let icon = weatherMarkers[weatherData.cityName].marker.getIcon();
+                // Should define a function that returns the URL below instead of hardcoding it
+                icon.url = "http://openweathermap.org/img/w/" + weatherData.weather.icon + ".png";
+                weatherMarkers[weatherData.cityName].marker.setIcon(icon);
+            }
         }
-    }
-};
+    };
 
-function myMap() 
-{
-    
-    // geocoder.geocode({'address':"British Columbia"}, (result, status) => {
-    //         if(status === google.maps.GeocoderStatus.OK)
-    //         {
-    //             alert("HERE!");
-    //             alert(JSON.stringify(result[0].geometry.location));
-                
-    //         }
-    //         else
-    //         {
-    //             alert("ERROR!");
-    //         }
-    // });
-    
-    // let geocoder = new google.maps.Geocoder();
-    // const locations = ["Dease Lake", "Fort Nelson", "Terrace", "Prince George", "Whistler", "Revelstoke", "Creston"];
-   
-    // geocoder.geocode({'address':"British Columbia"}, (result, status) => {
-    //     if(status === google.maps.GeocoderStatus.OK)
-    //     {
-    //         let mapProp = {
-    //             center:result[0].geometry.location,
-    //             zoom:5,
-    //         };
-    //         let map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    //         locations.forEach( (location) => {
-    //             geocoder.geocode({'address':location + ",BC"}, (results, status) => {
-    //                 let marker = new google.maps.Marker({position:results[0].geometry.location});
-    //                 marker.setMap(map);
-    //             });
-    //         });
-    //     }
-    //     else{
-    //         alert("Fail!");
-    //     }
-    // });   
-    // let citiesMap = {
-    //     "bc-74" : "Vancouver",
-    //     "bc-70" : "Agassiz",
-    // };
-    // $.ajax({
-    //     url: "/test",
-    //     type: "GET",
-    //     data: {locations:["bc-74", "bc-70"]}
-    // }).done((data) => {
-
-    //     alert(data);
-        // data.forEach( (element) => {
-        //     let cityName = citiesMap[element.city];
-        //     for( const entry of element.entries)
-        //     {
-        //         if( entry == "Current Conditions")
-        //         {
-        //             alert(entry);
-        //             break;
-        //         }
-        //     }      
-        // });
-    // });
-}
+});
